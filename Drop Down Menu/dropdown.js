0f0eaade67e779e64533a896fe3c6372814cca5a
Mtalha10k <trainedbot10k@gmail.com>
2024-10-27 15:40:51 +0530

    new

--- Drop Down Menu/dropdown.js
+++ Drop Down Menu/dropdown.js
@@ -1,19 +1,20 @@
 var selectField = document.getElementById('selectField');
-var selectText = document.getElementById('js-select-text'); 
+var selectText = document.getElementById('js-select-text'); // Ensurs the HTML
 var options = document.getElementsByClassName('options');
 var list = document.getElementById('list');
 var arrowIcon = document.getElementById('arrowIcon');
 
 
-selectField.onclick = function() {
+selectField.addEventListener('click', function() {
     list.classList.toggle('js-hide');
     arrowIcon.classList.toggle('arrow-icon-rotate');
-}
+});
+
 
 Array.from(options).forEach(option => {
-    option.onclick = function() {
+    option.addEventListener('click', function() {
         selectText.textContent = this.textContent;
-        list.classList.add('js-hide');
+        list.classList.add('js-hide'); 
         arrowIcon.classList.remove('arrow-icon-rotate'); 
-    }
+    });
 });
