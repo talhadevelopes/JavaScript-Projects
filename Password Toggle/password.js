99c43851362340f82a02c1edc57826777a308413
Mtalha10k <trainedbot10k@gmail.com>
2024-10-28 22:36:21 +0530

    New

--- Password Toggle/password.js
+++ Password Toggle/password.js
@@ -1,6 +1,9 @@
 const eyeIcon = document.getElementById('js-icon');
-const password = document.getElementById('js-password');
+const password = document.getElementById('js-password'); //
+var passwordMessage = document.getElementById('password-message');
+var passwordMessageStrength = document.getElementById('password-message-strength');
 
+password.addEventListener('input', displayPasswordMessage);
 eyeIcon.addEventListener('click', changeEyeIcon);
 
 function changeEyeIcon() {
@@ -14,3 +17,26 @@ function changeEyeIcon() {
         eyeIcon.classList.add('fa-eye-slash');
     }
 }
+
+function displayPasswordMessage() {
+    if (password.value.length > 0) {
+        passwordMessage.style.display = 'block';
+    } else {
+        passwordMessage.style.display = 'none';
+    }
+
+    if (password.value.length < 4) {
+        passwordMessageStrength.innerHTML = 'weak';
+        password.style.borderColor = '#ff5925';
+        passwordMessageStrength.style.color = '#ff5925';
+    } else if (password.value.length >= 4 && password.value.length < 8) {
+        passwordMessageStrength.innerHTML = 'medium';
+        password.style.borderColor = 'yellow';
+        passwordMessageStrength.style.color = 'yellow';
+    } else {
+        passwordMessageStrength.innerHTML = 'strong';
+        password.style.borderColor = '#26d730';
+        passwordMessageStrength.style.color = '#26d730';
+    }
+};
+
