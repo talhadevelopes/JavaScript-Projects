5620cc53600464544ffc0efc2e9c2b581a2dfdf1
Mtalha10k <trainedbot10k@gmail.com>
2024-10-28 22:03:01 +0530

    new

--- QR Code Generator/QRCode.js
+++ QR Code Generator/QRCode.js
@@ -1,11 +1,10 @@
 let imgBox = document.getElementById('img-Box');
 let qrImage = document.getElementById('qr-Image');
 let qrText = document.getElementById('qr-Text');
+
 let generateQrCodeButton = document.getElementById('generate-qr-code-button');
 
 generateQrCodeButton.addEventListener('click', generateQR);
-
-
 
 function generateQR() {
     if (qrText.value.length > 0) {
